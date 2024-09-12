f03e8fc962234f82f22b83c0bc27f6a3fdbb9ed0
Tyler Sherman <67756633+TShermz@users.noreply.github.com>
2024-09-12 16:22:59 -0400

    update to white text

--- client/src/pages/WeeklyRecapPage.jsx
+++ client/src/pages/WeeklyRecapPage.jsx
@@ -2,10 +2,13 @@ import PageContent from "../components/UI/PageContent";
 import { getWeeklyPost } from "../util/http";
 
 export default function WeeklyRecapPage() {
+  // const selectedWeek = useSelector((state) => state.general.selectedWeek);
   getWeeklyPost("4ZF6KaZicK2yWOY0i2ILoh");
 
   return (
     <PageContent>
+      <h2>Week 1</h2>
+
       <div className="weeklyRecap" id="weeklyRecap"></div>
     </PageContent>
   );
